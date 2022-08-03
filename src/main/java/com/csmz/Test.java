bb7db7d081a783455b9a7353f1e7c44fc3145512
JJGW <JJGW@csmz.com>
2022-08-03 20:54:19 +0800

    version-1.4

--- src/main/java/com/csmz/Test.java
+++ src/main/java/com/csmz/Test.java
@@ -10,5 +10,6 @@ public class Test {
         System.out.println("MASTER NB");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("result!");
     }
 }
