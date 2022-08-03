51ec095756bc28f2a3365f1c547a19bd85fb7c91
JJGW <JJGW@csmz.com>
2022-08-03 20:13:40 +0800

    hot-fix version-1.0

--- src/main/java/com/csmz/Test.java
+++ src/main/java/com/csmz/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("Hello Git!");
         System.out.println("你好 Git!");
         System.out.println("你薅 Git!");
+        System.out.println("你薅");
     }
 }
