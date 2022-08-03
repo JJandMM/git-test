fe3d51f13941d9e7cd934e745bc62a943e4b759c
JJGW <JJGW@csmz.com>
2022-08-03 20:07:49 +0800

    version-1.2

--- src/main/java/com/csmz/Test.java
+++ src/main/java/com/csmz/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("你好 Git!");
+        System.out.println("你薅 Git!");
     }
 }
